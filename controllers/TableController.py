161c4e22f5588b3724684257c78b5e764e0e292e
DmitryAverin <hamster@daotech.ru>
2019-05-21 23:26:14 +0400

    Add analytics

--- controllers/TableController.py
+++ controllers/TableController.py
@@ -1,4 +1,8 @@
+import math
 import os
+
+import pandas as pd
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 from helpers.ConstHolder import TermSetting
 from helpers.FolderParser import FolderParser
@@ -11,8 +15,12 @@ class TableController:
     graphic_helper = GraphicHelper()
     term_helper = TermHelper()
 
+    # list of lists. Every list contains filename, trend, season, rand component
     def get_table_data(self):
-        return self.folder_parser.parse()
+        real_data = self.folder_parser.parse()
+        for i in range(0, len(real_data)):
+            real_data[i] = self.analyze_components(real_data[i])
+        return real_data
 
     def show_graphic(self, filename):
         self.graphic_helper.points_y = []
@@ -35,7 +43,7 @@ class TableController:
             os.system("open '" + path_to_file + "'")
             # os.system("start " + path_to_file)
 
-    def terms_to_graphic_data(self,  filename):
+    def terms_to_graphic_data(self, filename):
 
         terms = self.term_helper.get_terms(filename)
         graphics = []
@@ -46,3 +54,61 @@ class TableController:
             c = term[3]
             graphics.append([[0, TermSetting.MARGIN, 0], [a, b, c]])
         return graphics
+
+    def analyze_components(self, components):
+        x, y = self.folder_parser.get_points_from_file_by_filename(components[0])
+        data_frame = pd.DataFrame(data=y)
+        print(data_frame)
+        decomposed = seasonal_decompose(data_frame, model='additive', freq=100)
+        print(components[0])
+        trend_component = self.prepare_dataframe(decomposed.trend)
+        season_component = self.prepare_dataframe(decomposed.seasonal)
+        random_component = self.prepare_dataframe(decomposed.resid)
+
+        if self.have_trend(trend_component, max(y)):
+            components.append("+")
+        else:
+            components.append("-")
+
+        if self.have_seasons(season_component, max(y)):
+            components.append("+")
+        else:
+            components.append("-")
+
+        if self.have_random(random_component, max(y)):
+            components.append("+")
+        else:
+            components.append("-")
+
+        return components
+
+    def prepare_dataframe(self, data_frame):
+        data = data_frame.ix[:, 0].tolist()
+        for i in range(0, len(data_frame)):
+            if math.isnan(data[i]):
+                data[i] = 0
+        return data
+
+    def have_trend(self, data_frame, max_value):
+        if max(data_frame) == 0:
+            return False
+        if max(data_frame)/max_value > 0.3:
+            return True
+        else:
+            return False
+
+    def have_seasons(self, data_frame, max_value):
+        if max(data_frame) == 0:
+            return False
+        if max(data_frame)/max_value  > 0.3:
+            return True
+        else:
+            return False
+
+    def have_random(self, data_frame, max_value):
+        if max(data_frame) == 0:
+            return False
+        if max(data_frame)/max_value  > 0.3:
+            return True
+        else:
+            return False
